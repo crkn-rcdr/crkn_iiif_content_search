4b8541c9f679fdeed827fa1fa3d00a423994de94
Brittny Lapierre <brittny.lapierre@gmail.com>
2026-02-04 12:20:22 -0500

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,5 +58,6 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.hosts << 'localhost:3000'
   config.hosts << 'crkn-iiif-content-search.azurewebsites.net'
 end
